a326a182d9af582885498923f95688d8055ec038
MalignisTormentum <89428243+MalignisTormentum@users.noreply.github.com>
2022-03-20 14:12:18 -0500

    Update db_connection.php

--- model/db_connection.php
+++ model/db_connection.php
@@ -1,9 +1,9 @@
 <?php
     try{
         $db = new PDO( 
-            'mysql:host=localhost;dbname=zippyusedautos;',
-            'root', 
-            ''
+            'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=on12dvmjkrfkmayo;',
+            'aahwcrj0o404oo39', 
+            'ser6bfs6wol01zal'
         );
     } catch(PDOException $e) {
         echo $e->getMessage();
